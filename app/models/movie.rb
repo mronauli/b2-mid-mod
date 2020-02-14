fbaae62c5c541eff82ef94efa6f2f2bfa6e2550e
Maria Ronauli <mronauli@gmail.com>
2020-02-14 10:22:07 -0700

    edit movie class

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,7 +1,6 @@
 class Movie < ApplicationRecord
   validates_presence_of :name, :year_created, :genre
   belongs_to :studio
-  has_many :actors
   has_many :actor_movies
-  has_many :actor, through: :actor_movies
+  has_many :actors, through: :actor_movies
 end
