6d77d24989f865e48ded2bcd53b4c3303003ed91
Maria Ronauli <mronauli@gmail.com>
2020-02-14 10:33:48 -0700

    add test for user strory 1

---
+++ spec/features/visitor_can_see_all_studios_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe 'on a studio index page' do
+    describe "as a visitor" do
+      it "can see all the studios a list of all the movies from each studio" do
+        studio_1 = Studio.create(name: "Cinemark")
+        studio_2 = Studio.create(name: "Warner Bros")
+        movie_1 = Movie.create(name: "Big Dogs", year_created: 1997, :genre "comedy", studio: studio_1)
+        movie_2 = Movie.create(name: "Scary Movie", year_created: 2000, :genre "horror", studio: studio_2)
+        movie_3 = Movie.create(name: "Funny Movie", year_created: 2000, :genre "comedy", studio: studio_2)
+
+        visit "/studios"
+        within("#studio-#{studio_1.id}") do
+          expect(page).to have_content(studio_1.name)
+          expect(page).to have_content(movie_1.name)
+        end
+        within("#studio-#{studio_2.id}") do
+          expect(page).to have_content(studio_2.name)
+          expect(page).to have_content(movie_2.name)
+          expect(page).to have_content(movie_3.name)
+        end
+      end
+    end
+  end
+# Story 1
+# As a user,
+# When I visit the studio index page
+# I see a list of all of the movie studios
+# And underneath each studio, I see the names of all of its movies.
+# Story 2
+# As a user,
