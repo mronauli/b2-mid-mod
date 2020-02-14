43e94995d5a11c54618d5b0a0d75fc4480b45901
Maria Ronauli <mronauli@gmail.com>
2020-02-14 10:45:28 -0700

    add route view and action for studio index

--- spec/features/visitor_can_see_all_studios_spec.rb
+++ spec/features/visitor_can_see_all_studios_spec.rb
@@ -5,9 +5,9 @@ RSpec.describe 'on a studio index page' do
       it "can see all the studios a list of all the movies from each studio" do
         studio_1 = Studio.create(name: "Cinemark")
         studio_2 = Studio.create(name: "Warner Bros")
-        movie_1 = Movie.create(name: "Big Dogs", year_created: 1997, :genre "comedy", studio: studio_1)
-        movie_2 = Movie.create(name: "Scary Movie", year_created: 2000, :genre "horror", studio: studio_2)
-        movie_3 = Movie.create(name: "Funny Movie", year_created: 2000, :genre "comedy", studio: studio_2)
+        movie_1 = Movie.create(name: "Big Dogs", year_created: 1997, genre: "comedy", studio: studio_1)
+        movie_2 = Movie.create(name: "Scary Movie", year_created: 2000, genre: "horror", studio: studio_2)
+        movie_3 = Movie.create(name: "Funny Movie", year_created: 2000, genre: "comedy", studio: studio_2)
 
         visit "/studios"
         within("#studio-#{studio_1.id}") do
